2a8b7c9a970df9579f612c04121181a07b21df21
perkles <douglasribeirosanguino3@gmail.com>
2021-07-21 21:38:05 -0300

    TesteOperacoes.kt

--- TesteOperacoes.kt
+++ TesteOperacoes.kt
@@ -16,4 +16,12 @@ fun main() {
     println("Os salários maiores que R$2000.0 são de:")
     val salariosmaiorque2k = salarios.filter { it > 2000.0 }
     salariosmaiorque2k.forEach { println(it) }
+
+    println("")
+
+    println(salarios.count { it in 2000.0..3500.0 }) //.count conta e mostra quantos salarios entre 2k e 3.5k
+    println("")
+    println(salarios.find { it == 2500.0 })
+    println("")
+    println(salarios.any { it == 100.0 })
 }
